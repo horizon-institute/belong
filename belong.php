957e47a2e76281ce3c7950c3cf3ec7f00bd6990b
Javid Yousaf <javidyousaf@Javids-MacBook-Pro.local>
2017-04-26 12:03:23 +0100

    Add Bitbucket Plugin URI to header

--- belong.php
+++ belong.php
@@ -4,7 +4,8 @@
 /**
 * Plugin Name: Belong
 * Plugin URI: http://belong-horizon.cloudapp.net
-* Description: Custom functionality for Belong CRM
+* Bitbucket Plugin URI: https://javidyousaf@bitbucket.org/javidyousaf/belong.git
+* Description: Custom functionality for Belong Nottingham CRM
 * Version: 0.1
 * Author: Javid Yousaf
 * License: GPL3
