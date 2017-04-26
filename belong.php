e7165a3ebc50a861a2ae58905e1056e90ef89b4a
Javid Yousaf <javidyousaf@Javids-MacBook-Pro.local>
2017-04-26 11:37:57 +0100

    initial commit

---
+++ belong.php
@@ -0,0 +1,15 @@
+
+<?php
+/**
+* Plugin Name: Belong
+* Plugin URI: http://belong-horizon.cloudapp.net
+* Description: Custom functionality for Belong CRM
+* Version: 0.1
+* Author: Javid Yousaf
+* License: GPL3
+*/
+
+// Prevent direct access
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+?>
