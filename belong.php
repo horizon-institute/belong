2d38726bfbdcce48b1e9b11faab15329b3cc862d
Javid Yousaf <javidyousaf@Javids-MacBook-Pro.local>
2017-04-27 09:48:51 +0100

    add staff role

--- belong.php
+++ belong.php
@@ -6,7 +6,7 @@
 * Plugin URI: http://belong-horizon.cloudapp.net
 * Bitbucket Plugin URI: https://javidyousaf@bitbucket.org/javidyousaf/belong.git
 * Description: Custom functionality for Belong Nottingham CRM
-* Version: 0.0.0.1
+* Version: 0.0.0.2
 * Author: Javid Yousaf
 * License: GPL3
 */
@@ -16,7 +16,17 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 // Add a Client user role that can only view their content
 function add_roles_on_plugin_activation() {
-    add_role( 'client', 'Client', array( 'read' => true ) );
+    add_role( 'client', 'Client', array( 
+        'read' => true 
+        )
+    );
+
+    add_role( 'staff', 'Staff', array( 
+        'read' => true,
+        'edit_posts' => true,
+        'delete_posts' => false,   
+        )
+    );
 }
 
 register_activation_hook( __FILE__, 'add_roles_on_plugin_activation' );
