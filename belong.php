75623f7f6f074bb6c44052f81ba3a2030cf32743
jav <javidyousaf@outlook.com>
2017-05-25 10:46:16 +0100

    More formatting

--- belong.php
+++ belong.php
@@ -6,7 +6,7 @@
 * Plugin URI: http://belong-horizon.cloudapp.net
 * Bitbucket Plugin URI: https://javidyousaf@bitbucket.org/javidyousaf/belong.git
 * Description: Custom functionality for Belong Nottingham CRM
-* Version: 0.0.1.5
+* Version: 0.0.1.6
 * Author: Javid Yousaf
 * License: GPL3
 */
@@ -24,7 +24,7 @@ function add_roles_on_plugin_activation() {
     add_role( 'staff', 'Staff', array( 
         'read' => true,
         'edit_posts' => true,
-        'delete_posts' => false,   
+        'delete_posts' => false,    //container
         )
     );
 }
@@ -41,17 +41,16 @@ function belong_list_events_for_user() {
 
     $posts = get_posts($args);
     ob_start();
-    ?><div class="container"><?php
-           
+    ?><div class="container"><?php     
     foreach ($posts as $post) {
         $event_client = get_field('event_client', $post->ID);
         if ($event_client['ID'] == $current_user->ID) {
             $event_date = get_field('event_date', $post->ID);
             $event_address = get_field('event_address', $post->ID);
-            echo '<div class="row"><h3>'.$post->post_title.'</h3>';
-            echo '<h5>'.$post->post_content.'</h5>';
-            echo '<h5>'.$event_date.'</h5>';
-            echo '<h5>'.$event_address.'</h5></div>';
+            echo '<div class="row"><h4>'.$post->post_title.'</h4>';
+            echo $post->post_content.'<br />';
+            echo $event_date.'<br />';
+            echo $event_address.'</div>';
         }
         echo '<br />';
     }
@@ -71,18 +70,20 @@ function belong_list_modules_for_user() {
 
     $posts = get_posts($args);
     ob_start();
+    ?><div class="container"><?php
     foreach ($posts as $post) {
         $module_client = get_field('module_client', $post->ID);
         if ($module_client['ID'] == $current_user->ID) {
             $module_complete_by = get_field('module_complete_by', $post->ID);
             $module_address = get_field('module_address', $post->ID);
-            echo '<h3>'.$post->post_title.'</h3>';
-            echo '<h5>'.$post->post_content.'</h5>';
-            echo '<h5>'.$module_complete_by.'</h5>';
-            echo '<h5>'.$module_address.'</h5>';
+            echo '<div class="row"><h4>'.$post->post_title.'</h4>';
+            echo $post->post_content.'<br />';
+            echo $module_complete_by.'<br />';
+            echo $module_address.'</div>';
         }
         echo '<br />';
     }
+    ?></div><?php
     return ob_get_clean();
 }
 
