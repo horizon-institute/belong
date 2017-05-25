c9e215d11a7e4a572a55c7963fb53351b1bd65fe
jav <javidyousaf@outlook.com>
2017-05-25 10:14:19 +0100

    Add module display function

--- belong.php
+++ belong.php
@@ -6,7 +6,7 @@
 * Plugin URI: http://belong-horizon.cloudapp.net
 * Bitbucket Plugin URI: https://javidyousaf@bitbucket.org/javidyousaf/belong.git
 * Description: Custom functionality for Belong Nottingham CRM
-* Version: 0.0.1.2
+* Version: 0.0.1.3
 * Author: Javid Yousaf
 * License: GPL3
 */
@@ -57,5 +57,33 @@ function belong_list_events_for_user() {
 }
 
 add_shortcode('user_events', 'belong_list_events_for_user');
+
+
+function belong_list_modules_for_user() {
+    $current_user = wp_get_current_user();
+    $args = array(
+        'posts_per_page'   => -1,
+        'post_type'        => 'modules'
+    );
+
+    $posts = get_posts($args);
+    ob_start();
+    foreach ($posts as $post) {
+        $module_clients = get_field('module_client', $post->ID);
+
+        if (in_array($current_user->ID, $module_clients['ID'])) {
+            $module_complete_by = get_field('module_complete_by', $post->ID);
+            $module_address = get_field('module_address', $post->ID);
+            echo '<h3>'.$post->post_title.'</h3>';
+            echo '<h5>'.$post->post_content.'</h5>';
+            echo '<h5>'.$module_complete_by.'</h5>';
+            echo '<h5>'.$module_address.'</h5>';
+        }
+        echo '<br />';
+    }
+    return ob_get_clean();
+}
+
+add_shortcode('user_modules', 'belong_list_modules_for_user');
 
 ?>
