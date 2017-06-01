6d1e757305859b8e5434477e48fffbf970908c7c
Javid yousaf <javidyousaf@outlook.com>
2017-06-01 19:11:37 +0100

    sss

--- belong.php
+++ belong.php
@@ -6,7 +6,7 @@
 * Plugin URI: http://belong-horizon.cloudapp.net
 * Bitbucket Plugin URI: https://javidyousaf@bitbucket.org/javidyousaf/belong.git
 * Description: Custom functionality for Belong Nottingham CRM
-* Version: 0.0.2.1
+* Version: 0.0.2.2
 * Author: Javid Yousaf
 * License: GPL3
 */
