d5c04f71d6b5073d66453b9bd727a6abf5c8acdb
jav <javidyousaf@outlook.com>
2017-06-01 13:05:19 +0100

    Add Agency role type

--- belong.php
+++ belong.php
@@ -6,7 +6,7 @@
 * Plugin URI: http://belong-horizon.cloudapp.net
 * Bitbucket Plugin URI: https://javidyousaf@bitbucket.org/javidyousaf/belong.git
 * Description: Custom functionality for Belong Nottingham CRM
-* Version: 0.0.1.8
+* Version: 0.0.1.9
 * Author: Javid Yousaf
 * License: GPL3
 */
@@ -14,13 +14,15 @@
 // Prevent direct access
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-// Add a Client user role that can only view their content
+/* Theses roles are further modified by the "Members" role management plugin */
 function add_roles_on_plugin_activation() {
+    /* Client user role can only view their assigned content. */
     add_role( 'client', 'Client', array( 
         'read' => true 
         )
     );
 
+    /* Staff role can created / edit / delete Modules, Events, Users and Assignments. */
     add_role( 'staff', 'Staff', array( 
         'read' => true,
         'edit_posts' => true,
@@ -31,10 +33,45 @@ function add_roles_on_plugin_activation() {
         'edit_users' => true   
         )
     );
+
+    /* External agency will be able to create Modules & Events and do assignments but
+    these will have to be approved by a Staff role. */
+    add_role( 'agency', 'Agency', array( 
+        'read' => true,
+        'edit_posts' => true,
+        'delete_posts' => false  
+        )
+    );
 }
 
 register_activation_hook( __FILE__, 'add_roles_on_plugin_activation' );
 
+
+// function belong_list_events_for_user() {
+//     $current_user = wp_get_current_user();
+//     $args = array(
+//         'posts_per_page'   => -1,
+//         'post_type'        => 'events'
+//     );
+
+//     $posts = get_posts($args);
+//     ob_start();
+//     ?><div class="container"><?php     
+//     foreach ($posts as $post) {
+//         $event_client = get_field('event_client', $post->ID);
+//         if ($event_client['ID'] == $current_user->ID) {
+//             $event_date = get_field('event_date', $post->ID);
+//             $event_address = get_field('event_address', $post->ID);
+//             echo '<div class="row"><h4>'.$post->post_title.'</h4>';
+//             echo $post->post_content.'<br />';
+//             echo $event_date.'<br />';
+//             echo $event_address.'</div>';
+//         }
+//         echo '<br />';
+//     }
+//     ?></div><?php
+//     return ob_get_clean();
+// }
 
 function belong_list_events_for_user() {
     $current_user = wp_get_current_user();
@@ -62,11 +99,64 @@ function belong_list_events_for_user() {
     return ob_get_clean();
 }
 
+
 add_shortcode('user_events', 'belong_list_events_for_user');
+
+
+
+// function belong_list_modules_for_user() {
+//     $current_user = wp_get_current_user();
+//     $args = array(
+//         'posts_per_page'   => -1,
+//         'post_type'        => 'modules'
+//     );
+
+//     $posts = get_posts($args);
+//     ob_start();
+//     ?><div class="container"><?php
+//     foreach ($posts as $post) {
+//         $module_client = get_field('module_client', $post->ID);
+//         if ($module_client['ID'] == $current_user->ID) {
+//             $module_complete_by = get_field('module_complete_by', $post->ID);
+//             $module_address = get_field('module_address', $post->ID);
+//             echo '<div class="row"><h4>'.$post->post_title.'</h4>';
+//             echo $post->post_content.'<br />';
+//             echo $module_complete_by.'<br />';
+//             echo $module_address.'</div>';
+//         }
+//         echo '<br />';
+//     }
+//     ?></div><?php
+//     return ob_get_clean();
+// }
 
 function belong_list_modules_for_user() {
     $current_user = wp_get_current_user();
-    $args = array(
+    $args = array(ction belong_list_modules_for_user() {
+//     $current_user = wp_get_current_user();
+//     $args = array(
+//         'posts_per_page'   => -1,
+//         'post_type'        => 'modules'
+//     );
+
+//     $posts = get_posts($args);
+//     ob_start();
+//     ?><div class="container"><?php
+//     foreach ($posts as $post) {
+//         $module_client = get_field('module_client', $post->ID);
+//         if ($module_client['ID'] == $current_user->ID) {
+//             $module_complete_by = get_field('module_complete_by', $post->ID);
+//             $module_address = get_field('module_address', $post->ID);
+//             echo '<div class="row"><h4>'.$post->post_title.'</h4>';
+//             echo $post->post_content.'<br />';
+//             echo $module_complete_by.'<br />';
+//             echo $module_address.'</div>';
+//         }
+//         echo '<br />';
+//     }
+//     ?></div><?php
+//     return ob_get_clean();
+// }
         'posts_per_page'   => -1,
         'post_type'        => 'modules'
     );
@@ -77,7 +167,31 @@ function belong_list_modules_for_user() {
     foreach ($posts as $post) {
         $module_client = get_field('module_client', $post->ID);
         if ($module_client['ID'] == $current_user->ID) {
-            $module_complete_by = get_field('module_complete_by', $post->ID);
+            $module_complete_by = get_field('module_complete_by', ction belong_list_modules_for_user() {
+//     $current_user = wp_get_current_user();
+//     $args = array(
+//         'posts_per_page'   => -1,
+//         'post_type'        => 'modules'
+//     );
+
+//     $posts = get_posts($args);
+//     ob_start();
+//     ?><div class="container"><?php
+//     foreach ($posts as $post) {
+//         $module_client = get_field('module_client', $post->ID);
+//         if ($module_client['ID'] == $current_user->ID) {
+//             $module_complete_by = get_field('module_complete_by', $post->ID);
+//             $module_address = get_field('module_address', $post->ID);
+//             echo '<div class="row"><h4>'.$post->post_title.'</h4>';
+//             echo $post->post_content.'<br />';
+//             echo $module_complete_by.'<br />';
+//             echo $module_address.'</div>';
+//         }
+//         echo '<br />';
+//     }
+//     ?></div><?php
+//     return ob_get_clean();
+// }$post->ID);
             $module_address = get_field('module_address', $post->ID);
             echo '<div class="row"><h4>'.$post->post_title.'</h4>';
             echo $post->post_content.'<br />';
