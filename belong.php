82cf8e4d7f1b6ff429eab3d5c9b4fde38ea89c2e
jav <javidyousaf@outlook.com>
2017-06-15 13:27:49 +0100

    user stuff

--- belong.php
+++ belong.php
@@ -6,7 +6,7 @@
 * Plugin URI: http://belong-horizon.cloudapp.net
 * Bitbucket Plugin URI: https://javidyousaf@bitbucket.org/javidyousaf/belong.git
 * Description: Custom functionality for Belong Nottingham CRM
-* Version: 0.0.5.6
+* Version: 0.0.5.7
 * Author: Javid Yousaf
 * License: GPL3
 */
@@ -43,7 +43,7 @@ add_shortcode('user_events', 'belong_list_events_for_user');
 /*********************************************************************************/
 function belong_list_modules_for_user() {
     ob_start();
-    $current_user = wp_get_current_user();user_login
+    $current_user = wp_get_current_user();
     $args = array(
         'posts_per_page'   => -1,
         'post_type'        => 'assignments'
@@ -72,9 +72,10 @@ add_shortcode('user_modules', 'belong_list_modules_for_user');
 function belong_show_user_info_main() {
     ob_start();
     $user_info = wpse_58429();
-    echo 'Username: ' . $user_info->user_login . "\n";
-    echo 'User roles: ' . implode(', ', $user_info->roles) . "\n";
-    echo 'User ID: ' . $user_info->ID . "\n";
+    echo 'Hi ' . $user_info->display_name . "<br />";
+    echo 'First Name: ' . $user_info->user_firstname . "<br />";
+    echo 'Last Name: ' . $user_info->user_lastname . "<br />";
+    echo 'Email: ' . $user_info->user_email . "<br />";
     return ob_get_clean();
 }
 
