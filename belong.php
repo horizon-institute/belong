5b60f5528ae2b7d13f7ee3c10ae80328fd15b674
jav <javidyousaf@outlook.com>
2017-06-15 08:36:08 +0100

    removed roles

--- belong.php
+++ belong.php
@@ -6,7 +6,7 @@
 * Plugin URI: http://belong-horizon.cloudapp.net
 * Bitbucket Plugin URI: https://javidyousaf@bitbucket.org/javidyousaf/belong.git
 * Description: Custom functionality for Belong Nottingham CRM
-* Version: 0.0.5.1
+* Version: 0.0.5.2
 * Author: Javid Yousaf
 * License: GPL3
 */
@@ -14,63 +14,24 @@
 // Prevent direct access
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-/* Theses roles are further modified by the "Members" role management plugin */
-function add_roles_on_plugin_activation() {
-    /* Client user role can only view their assigned content. */
-    add_role( 'client', 'Client', array( 
-        'read' => true 
-        )
-    );
-
-    /* Staff role can created / edit / delete Modules, Events, Users and Assignments. */
-    add_role( 'staff', 'Staff', array( 
-        'read' => true,
-        'edit_posts' => true,
-        'delete_posts' => false,
-        'create_users' => true,
-        'delete_users' => true,
-        'list_users' => true,
-        'edit_users' => true   
-        )
-    );
-
-    /* Access to own input clients with right to edit own.
-        Compile reports based accessible clients. */
-    add_role( 'agency', 'Agency', array( 
-        'read' => true,
-        'edit_posts' => true,
-        'delete_posts' => false  
-        )
-    );
-
-    /* Access specific clients (preferable by number not name) review client profile 
-        and activity, create reports and print. No editing client info */
-    add_role( 'funder', 'Funder', array( 
-        'read' => true,
-        'edit_posts' => true,
-        'delete_posts' => false  
-        )
-    );
-}
-
-register_activation_hook( __FILE__, 'add_roles_on_plugin_activation' );
-
 /*********************************************************************************/
 function belong_list_events_for_user() {
     ob_start();
     $current_user = wp_get_current_user();
-    $args = array(table
+    $args = array(
         'posts_per_page'   => -1,
         'post_type'        => 'assignments'
     );
 
     $event_posts = get_posts($args);  
-    foreach ($event_posts as $post) {
-        $assignment_client = get_field('assignment_client', $post->ID);
-        $assignment_type = get_field('assignment_type', $post->ID);
-        if ($assignment_client['ID'] == $current_user->ID && $assignment_type == 'Events') {
-            $assignment_select_event = get_field('assignment_select_event', $post->ID); 
-            ?><h4><a href="<?php get_permalink($post->ID); ?>"><?php echo $post->post_title; ?></a></h4><?php
+    if ($event_posts) {
+        foreach ($event_posts as $post) {
+            $assignment_client = get_field('assignment_client', $post->ID);
+            $assignment_type = get_field('assignment_type', $post->ID);
+            if ($assignment_client['ID'] == $current_user->ID && $assignment_type == 'Events') {
+                $assignment_select_event = get_field('assignment_select_event', $post->ID); 
+                ?><h4><a href="<?php get_permalink($post->ID); ?>"><?php echo $post->post_title; ?></a></h4><?php
+            }
         }
     }
     return ob_get_clean();
@@ -86,32 +47,21 @@ function belong_list_modules_for_user() {
     $args = array(
         'posts_per_page'   => -1,
         'post_type'        => 'assignments'
-    )
-    ?>
-    <div class="row">
-    <?php 
+    );
+
     $module_posts = get_posts($args);
-    if($module_posts):
-        foreach ($module_posts as $post) :
+    if ($module_posts) {
+        foreach ($module_posts as $post) {
             $assignment_client = get_field('assignment_client', $post->ID);
             $assignment_type = get_field('assignment_type', $post->ID);
-            if ($assignment_client['ID'] == $current_user->ID && $assignment_type == 'Modules') :
+            if ($assignment_client['ID'] == $current_user->ID && $assignment_type == 'Modules') {
                 $assignment_complete_by = get_field('assignment_complete_by', $post->ID);
+                $assignment_select_event = get_field('assignment_select_event', $post->ID); 
                 $assignment_select_module = get_field('assignment_select_module', $post->ID);
-                ?>
-                <ul>
-                    <li><a href="<?php echo get_permalink($post->ID) ?>"><?php echo $post->post_title; ?></a></li>
-                    <li><?php echo $assignment_complete_by; ?></li>
-                    <li><?php echo $assignment_select_module; ?></li>
-                </ul>
-                <?php 
-            endif;
-        endforeach; 
-        wp_reset_postdata();
-    endif;
-        ?>
-     </div>
-    <?php 
+                ?><h4><a href="<?php get_permalink($post->ID); ?>"><?php echo $post->post_title; ?></a></h4><?php
+            }
+        }
+    }
     return ob_get_clean();
 }
 
