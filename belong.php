7dcd81fac12fed6c9397d0976c7581461c8ef1aa
jav <javidyousaf@outlook.com>
2017-06-15 15:09:09 +0100

    more tables

--- belong.php
+++ belong.php
@@ -6,7 +6,7 @@
 * Plugin URI: http://belong-horizon.cloudapp.net
 * Bitbucket Plugin URI: https://javidyousaf@bitbucket.org/javidyousaf/belong.git
 * Description: Custom functionality for Belong Nottingham CRM
-* Version: 0.0.6.0
+* Version: 0.0.6.1
 * Author: Javid Yousaf
 * License: GPL3
 */
@@ -56,9 +56,9 @@ function belong_list_modules_for_user() {
             $assignment_type = get_field('assignment_type', $post->ID);
             if ($assignment_client['ID'] == $current_user->ID && $assignment_type == 'Modules') {
                 $permalink = get_permalink($post->ID);
-                echo "Assignment: <a href='" .$permalink. "'>". $post->post_title . "</a><br />";
-                echo "Complete By: " . get_field('assignment_complete_by', $post->ID) . "<br />";
-                echo "Module: " . get_field('assignment_select_module', $post->ID) . "<br />";
+                echo "<table><tr><td>Assignment</td><td><a href='" .$permalink. "'>". $post->post_title . "</a></td></tr>";
+                echo "<tr><td>Complete By</td><td>" . get_field('assignment_complete_by', $post->ID) . "</td></tr>";
+                echo "<tr><td>Module</td><td>" . get_field('assignment_select_module', $post->ID) . "</td></tr></table>";
             }
         }
     }
@@ -72,10 +72,26 @@ add_shortcode('user_modules', 'belong_list_modules_for_user');
 function belong_show_user_info_main() {
     ob_start();
     $user_info = belong_get_logged_in_user_info();
-    echo "<table><tr><td>Hi</td><td>" . $user_info->display_name . "<td/></tr>";
-    echo "<tr><td>First Name</td><td>" . $user_info->user_firstname . "<td/></tr>";
-    echo "<tr><td>Last Name</td><td>" . $user_info->user_lastname . "<td/></tr>";
-    echo "<tr><td>Email</td><td>: " . $user_info->user_email . "<td/></tr></table>";
+    echo "<table><tr><td>Hi</td><td>" . $user_info->display_name . "</td></tr>";
+    echo "<tr><td>First Name</td><td>" . $user_info->user_firstname . "</td></tr>";
+    echo "<tr><td>Last Name</td><td>" . $user_info->user_lastname . "</td></tr>";
+    echo "<tr><td>Email</td><td>" . $user_info->user_email . "</td></tr></table>";
+
+    /*also need to include custom user fields:
+    client number / date of registration
+    interviewer / case owner
+    gender
+
+    address
+    telephone
+    postcode
+    accomodation type
+    nationality / code
+    married /civil partenership / common law / single
+    religion / place of worship
+    spouse partner name / client number / uk resident / did they travel with you?
+
+    */
     return ob_get_clean();
 }
 
