12f26a4d2dcc814ebf865d4676171e9c1d1f9aa3
jav <javidyousaf@outlook.com>
2017-06-16 09:12:38 +0100

    events table

--- belong.php
+++ belong.php
@@ -6,7 +6,7 @@
 * Plugin URI: http://belong-horizon.cloudapp.net
 * Bitbucket Plugin URI: https://javidyousaf@bitbucket.org/javidyousaf/belong.git
 * Description: Custom functionality for Belong Nottingham CRM
-* Version: 0.0.6.4
+* Version: 0.0.6.5
 * Author: Javid Yousaf
 * License: GPL3
 */
@@ -17,6 +17,7 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 /*********************************************************************************/
 function belong_list_events_for_user() {
     ob_start();
+    $counter = 0;
     $current_user = belong_get_logged_in_user_info();
     $args = array(
         'posts_per_page'   => -1,
@@ -25,14 +26,17 @@ function belong_list_events_for_user() {
 
     $event_posts = get_posts($args);  
     if ($event_posts) {
+        echo "<table>";
         foreach ($event_posts as $post) {
             $assignment_client = get_field('assignment_client', $post->ID);
             $assignment_type = get_field('assignment_type', $post->ID);
             if ($assignment_client['ID'] == $current_user->ID && $assignment_type == 'Events') {
-                $assignment_select_event = get_field('assignment_select_event', $post->ID); 
-                ?><h4><a href="<?php get_permalink($post->ID); ?>"><?php echo $post->post_title; ?></a></h4><?php
+                $permalink = get_permalink($post->ID);
+                echo "<tr><td>" . $counter . "</td><td><a href='" .$permalink. "'>". $post->post_title . "</a></td>";
+                echo "<td>Complete By</td><td>" . get_field('assignment_complete_by', $post->ID) . "</td></tr>";
             }
         }
+        echo "</table>";
     }
     return ob_get_clean();
 }
@@ -74,7 +78,7 @@ add_shortcode('user_modules', 'belong_list_modules_for_user');
 /*********************************************************************************/
 function belong_show_user_info_main() {
     ob_start();
-    $user_info = belong_get_logged_in_user_info();
+    $user_info = belong_get_user_info();
     echo "<table><tr><td>Hi</td><td>" . $user_info->display_name . "</td></tr>";
     echo "<tr><td>First Name</td><td>" . $user_info->user_firstname . "</td></tr>";
     echo "<tr><td>Last Name</td><td>" . $user_info->user_lastname . "</td></tr>";
@@ -100,7 +104,7 @@ function belong_show_user_info_main() {
 
 add_shortcode('user_profile_main', 'belong_show_user_info_main');
 
-function belong_get_logged_in_user_info() {
+function belong_get_user_info() {
     $current_user = wp_get_current_user(); 
     if (!($current_user instanceof WP_User)) 
         return; 
