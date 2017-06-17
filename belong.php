9fc3cdb347699a2df3e7ecee2e468e7af80b7b1b
jav <javidyousaf@outlook.com>
2017-06-17 11:28:42 +0100

    menu

--- belong.php
+++ belong.php
@@ -6,7 +6,7 @@
 * Plugin URI: http://belong-horizon.cloudapp.net
 * Bitbucket Plugin URI: https://javidyousaf@bitbucket.org/javidyousaf/belong.git
 * Description: Custom functionality for Belong Nottingham CRM
-* Version: 0.0.8.2
+* Version: 0.0.8.3
 * Author: Javid Yousaf
 * License: GPL3
 */
@@ -68,7 +68,18 @@ function belong_list_modules_for_user() {
                 echo "<td>Complete By</td><td>" . get_field('assignment_complete_by', $post->ID) . "</td></tr>";
             }
         }
-        echo "</table>";
+        echo "</table>";ction select_menu_for_role($args = '') {
+//     $user = belong_get_user_info();
+//     $role = get_users_role($user->ID);
+//     if ($role == 'Client') {
+//         $args['menu'] = 'client-top-menu';
+//     } else {
+//         $args['menu'] = 'staff-top-menu';
+//     }
+//     return $args;
+// }
+
+// add_filter('wp_nav_menu_args', 'select_menu_for_role');
     }
     return ob_get_clean();
 }
@@ -77,48 +88,47 @@ add_shortcode('user_modules', 'belong_list_modules_for_user');
 
 
 /*********************************************************************************/
-function belong_show_user_info_main() {
-    ob_start();
-    $user_info = belong_get_user_info();
-    echo "<table><tr><td>Display Name</td><td>" . $user_info->display_name . "</td></tr>";
-    echo "<tr><td>First Name</td><td>" . $user_info->user_firstname . "</td></tr>";
-    echo "<tr><td>Last Name</td><td>" . $user_info->user_lastname . "</td></tr>";
-    echo "<tr><td>Email</td><td>" . $user_info->user_email . "</td></tr></table>";
+// function belong_show_user_info_main() {
+//     ob_start();
+//     $user_info = belong_get_user_info();
+//     echo "<table><tr><td>Display Name</td><td>" . $user_info->display_name . "</td></tr>";
+//     echo "<tr><td>First Name</td><td>" . $user_info->user_firstname . "</td></tr>";
+//     echo "<tr><td>Last Name</td><td>" . $user_info->user_lastname . "</td></tr>";
+//     echo "<tr><td>Email</td><td>" . $user_info->user_email . "</td></tr></table>";
 
-    /*also need to include custom user fields:
-    client number / date of registration
-    interviewer / case owner
-    gender
+//     /*also need to include custom user fields:
+//     client number / date of registration
+//     interviewer / case owner
+//     gender
 
-    address
-    telephone
-    postcode
-    accomodation type
-    nationality / code
-    married /civil partenership / common law / single
-    religion / place of worship
-    spouse partner name / client number / uk resident / did they travel with you?
+//     address
+//     telephone
+//     postcode
+//     accomodation type
+//     nationality / code
+//     married /civil partenership / common law / single
+//     religion / place of worship
+//     spouse partner name / client number / uk resident / did they travel with you?
 
-    */
-    return ob_get_clean();
-}
+//     */
+//     return ob_get_clean();
+// }
 
-add_shortcode('user_profile_main', 'belong_show_user_info_main');
+// add_shortcode('user_profile_main', 'belong_show_user_info_main');
 
 /*********************************************************************************/
-function select_menu_for_role($args = '') {
-    $user = belong_get_user_info();
-    $role = get_users_role($user->ID);
-    echo $role;
-    if ($role == 'Client') {
-        $args['menu'] = 'client-top-menu';
-    } else {
-        $args['menu'] = 'staff-top-menu';
-    }
-    return $args;
-}
+// function select_menu_for_role($args = '') {
+//     $user = belong_get_user_info();
+//     $role = get_users_role($user->ID);
+//     if ($role == 'Client') {
+//         $args['menu'] = 'client-top-menu';
+//     } else {
+//         $args['menu'] = 'staff-top-menu';
+//     }
+//     return $args;
+// }
 
-add_filter('wp_nav_menu_args', 'select_menu_for_role');
+// add_filter('wp_nav_menu_args', 'select_menu_for_role');
 
 /***********************************
 *        HELPER FUNCTIONS          *
