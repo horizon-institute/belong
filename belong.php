0f250f2c6f2dcba57bf8c5027a761acb9ed84f4a
jav <javidyousaf@outlook.com>
2017-06-18 08:40:17 +0100

    filter menus

--- belong.php
+++ belong.php
@@ -6,13 +6,26 @@
 * Plugin URI: http://belong-horizon.cloudapp.net
 * Bitbucket Plugin URI: https://javidyousaf@bitbucket.org/javidyousaf/belong.git
 * Description: Custom functionality for Belong Nottingham CRM
-* Version: 0.0.8.4
+* Version: 0.0.8.5
 * Author: Javid Yousaf
 * License: GPL3
 */
 
 // Prevent direct access
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+add_filter('wp_nav_menu_args', 'select_menu_for_role');
+
+/*********************************************************************************/
+function select_menu_for_role($args = '') {
+    $user = belong_get_user_info();
+    $role = get_users_role($user->ID);
+    if ($role == 'Client') {
+        $args['menu'] = 'client-top-menu';
+    } else {
+        $args['menu'] = 'staff-top-menu';
+    }
+    return $args;
+}
 
 /*********************************************************************************/
 function belong_list_events_for_user() {
@@ -75,49 +88,6 @@ function belong_list_modules_for_user() {
 
 add_shortcode('user_modules', 'belong_list_modules_for_user');
 
-
-/*********************************************************************************/
-// function belong_show_user_info_main() {
-//     ob_start();
-//     $user_info = belong_get_user_info();
-//     echo "<table><tr><td>Display Name</td><td>" . $user_info->display_name . "</td></tr>";
-//     echo "<tr><td>First Name</td><td>" . $user_info->user_firstname . "</td></tr>";
-//     echo "<tr><td>Last Name</td><td>" . $user_info->user_lastname . "</td></tr>";
-//     echo "<tr><td>Email</td><td>" . $user_info->user_email . "</td></tr></table>";
-
-//     /*also need to include custom user fields:
-//     client number / date of registration
-//     interviewer / case owner
-//     gender
-
-//     address
-//     telephone
-//     postcode
-//     accomodation type
-//     nationality / code
-//     married /civil partenership / common law / single
-//     religion / place of worship
-//     spouse partner name / client number / uk resident / did they travel with you?
-
-//     */
-//     return ob_get_clean();
-// }
-
-// add_shortcode('user_profile_main', 'belong_show_user_info_main');
-
-/*********************************************************************************/
-// function select_menu_for_role($args = '') {
-//     $user = belong_get_user_info();
-//     $role = get_users_role($user->ID);
-//     if ($role == 'Client') {
-//         $args['menu'] = 'client-top-menu';
-//     } else {
-//         $args['menu'] = 'staff-top-menu';
-//     }
-//     return $args;
-// }
-
-// add_filter('wp_nav_menu_args', 'select_menu_for_role');
 
 /***********************************
 *        HELPER FUNCTIONS          *
