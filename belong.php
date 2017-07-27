b580c563ded4ea19cb943b723c8d0e0dd6ed1fcd
Javid Yousaf <javidyousaf@javids-mbp.home>
2017-07-27 16:30:12 +0100

    assignments

--- belong.php
+++ belong.php
@@ -5,7 +5,7 @@
 * Plugin URI: http://belong-horizon.cloudapp.net
 * Bitbucket Plugin URI: https://javidyousaf@bitbucket.org/javidyousaf/belong.git
 * Description: Custom functionality for Belong Nottingham CRM
-* Version: 0.1.0.8
+* Version: 0.1.0.9
 * Author: Javid Yousaf
 * License: GPL3
 */
@@ -54,21 +54,21 @@ function belong_list_events_for_user() {
     $counter = 0;
     $current_user = belong_get_user_info();
     $assignment_args = array(
-        'posts_per_page'   => -1,
-        'post_type'        => 'assignments'
+    'posts_per_page'   => -1,
+    'post_type'        => 'assignments'
     );
-
-    $assignment_posts = get_posts($assignment_args);  
-    var_dump($assignment_posts);
+    
+    $assignment_posts = get_posts($assignment_args);
     if ($assignment_posts) {
         echo "<table>";
         foreach ($assignment_posts as $post) {
             $assignment_client = get_field('assignment_client', $post->ID);
             $assignment_type = get_field('assignment_type', $post->ID);
+            $assignment_event = get_field('assignment_select_event', $post->ID);
             //var_dump($assignment_type);
             if (belong_is_current_user_selected($assignment_client, $current_user->ID) && $assignment_type == 'Events') {
                 $counter++;
-                $permalink = get_permalink($post->ID);
+                $permalink = get_permalink($assignment_event->ID);
                 echo "<tr><td>" . $counter . "</td><td><a href='" .$permalink. "'>". $post->post_title . "</a></td></tr>";
                 // echo "<td>Complete By</td><td>" . get_field('assignment_complete_by', $post->ID) . "</td></tr>";
             }
@@ -86,10 +86,10 @@ function belong_list_modules_for_user() {
     $counter = 0;
     $current_user = belong_get_user_info();
     $assignment_args = array(
-        'posts_per_page'   => -1,
-        'post_type'        => 'assignments'
+    'posts_per_page'   => -1,
+    'post_type'        => 'assignments'
     );
-
+    
     $module_posts = get_posts($assignment_args);
     if ($assignment_posts) {
         echo "<table>";
@@ -113,11 +113,11 @@ add_shortcode('user_modules', 'belong_list_modules_for_user');
 
 /***********************************
 *        HELPER FUNCTIONS          *
-************************************/ 
+************************************/
 
 /***********************************************
- Check if current user ID is in the mult-select 
- array for the particular assignment
+Check if current user ID is in the mult-select
+array for the particular assignment
 ***********************************************/
 function belong_is_current_user_selected(array $array, $id) {
     foreach ($array as $element) {
@@ -129,18 +129,18 @@ function belong_is_current_user_selected(array $array, $id) {
 }
 
 /***********************************************
- Get current user
+Get current user
 ************************************************/
 function belong_get_user_info() {
-    $current_user = wp_get_current_user(); 
-    if (!($current_user instanceof WP_User)) 
-        return; 
-    return $current_user; 
+    $current_user = wp_get_current_user();
+    if (!($current_user instanceof WP_User))
+        return;
+    return $current_user;
 }
 
 /***********************************************
- Get role of current logged in user 
- - only returns the first one.
+Get role of current logged in user
+- only returns the first one.
 ************************************************/
 function belong_get_users_role($user_id) {
     $user = new WP_User($user_id);
