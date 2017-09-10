8d63b2a38fc56e8a5fd55bae2df214d7c0f1bc32
Javid Yousaf <javidyousaf@Javids-MacBook-Pro.local>
2017-09-10 09:39:53 +0100

    s

--- belong.php
+++ belong.php
@@ -5,7 +5,7 @@
 * Plugin URI: http://belong-horizon.cloudapp.net
 * Bitbucket Plugin URI: https://javidyousaf@bitbucket.org/javidyousaf/belong.git
 * Description: Custom functionality for Belong Nottingham CRM
-* Version: 0.2.2.5
+* Version: 0.2.2.6
 * Author: Javid Yousaf
 * License: GPL3
 */
@@ -582,7 +582,7 @@ function child_field($child_no) {
 }
 
 function children() {
-    echo '<div class="col-md-12" >';
+    echo '<div class="col-md-12">';
     echo '<div class="panel panel-primary">';
     echo '<div class="panel-heading">CHILDREN</div>';
     echo '<div class="panel-body">';
@@ -642,7 +642,7 @@ function children() {
         var rdiv = 'removeclass' + child;
         //divtest.innerHTML = '<div class="col-md-3 nopadding"><div class="form-group"> <input type="text" class="form-control" id="pw-child-name" name="pw-child-name[]" value="" placeholder="CHILD NAME"></div></div><div class="col-md-3 nopadding"><div class="form-group"> <input type="text" class="form-control" id="pw-child-dob" name="pw-child-dob[]" value="" placeholder="pw-child-dob"></div></div><div class="col-md-3 nopadding"><div class="form-group"> <input type="text" class="form-control" id="pw-child-cn" name="pw-child-cn[]" value="" placeholder="pw-child-cn"></div></div><div class="col-md-3 nopadding"><div class="form-group"><div class="input-group"> <select class="form-control" id="pw-child-uk" name="pw-child-uk[]"><option value="">Date</option><option value="2015">2015</option><option value="2016">2016</option><option value="2017">2017</option><option value="2018">2018</option> </select><div class="input-group-btn"> <button class="btn btn-danger btn-sm" type="button" onclick="remove_child_fields(' + child + ');"> <span class="glyphicon glyphicon-minus" aria-hidden="true"></span></button></div></div></div></div><div class="clear"></div>';
         
-        divtest.innerHTML = '<div class="col-md-3 nopadding"><div class="form-group"><input type="text" class="form-control" id="pw-child-name" name="pw-child-name[]" value="" placeholder="CHILD NAME"></div></div><div class="col-md-3 nopadding"><?php datepicker("pw-child-dob[]");?><div class="form-group"><input type="text" class="form-control" id="pw-child-dob" name="pw-child-dob[]" value="" placeholder="DOB"></div></div><div class="col-md-3 nopadding"><div class="form-group"><input type="text" class="form-control" id="pw-child-cn" name="pw-child-cn[]" value="" placeholder="CLIENT NUMBER"></div></div><div class="col-md-3 nopadding"><div class="form-group"><div class="input-group"><select class="form-control" id="pw-child-uk" name="pw-child-uk[]"><option value="">UK?</option><option value="yes">YES</option><option value="no">NO</option></select><div class="input-group-btn"><button class="btn btn-danger btn-sm" type="button" onclick="remove_child_fields(' + child + ');"> <span class="glyphicon glyphicon-minus" aria-hidden="true"></span></button></div></div></div></div><div class="clear"></div></div>';
+        divtest.innerHTML = '<div class="col-md-3 nopadding"><div class="form-group"><input type="text" class="form-control" id="pw-child-name" name="pw-child-name[]" value="" placeholder="CHILD NAME"></div></div><div class="col-md-3 nopadding"><?php echo datepicker(pw-child-dob[]); ?><div class="form-group"><input type="text" class="form-control" id="pw-child-dob" name="pw-child-dob[]" value="" placeholder="DOB"></div></div><div class="col-md-3 nopadding"><div class="form-group"><input type="text" class="form-control" id="pw-child-cn" name="pw-child-cn[]" value="" placeholder="CLIENT NUMBER"></div></div><div class="col-md-3 nopadding"><div class="form-group"><div class="input-group"><select class="form-control" id="pw-child-uk" name="pw-child-uk[]"><option value="">UK?</option><option value="yes">YES</option><option value="no">NO</option></select><div class="input-group-btn"><button class="btn btn-danger btn-sm" type="button" onclick="remove_child_fields(' + child + ');"> <span class="glyphicon glyphicon-minus" aria-hidden="true"></span></button></div></div></div></div><div class="clear"></div></div>';
         
         
         objTo.appendChild(divtest)
