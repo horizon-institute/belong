b9c9378daaecab141ab40713e18a89e6253bc8c8
Javid Yousaf <javidyousaf@Javids-MacBook-Pro.local>
2017-09-13 17:49:53 +0100

    x

--- belong.php
+++ belong.php
@@ -5,7 +5,7 @@
 * Plugin URI: http://belong-horizon.cloudapp.net
 * Bitbucket Plugin URI: https://javidyousaf@bitbucket.org/javidyousaf/belong.git
 * Description: Custom functionality for Belong Nottingham CRM
-* Version: 0.2.7.4
+* Version: 0.2.7.5
 * Author: Javid Yousaf
 * License: GPL3
 */
@@ -29,21 +29,15 @@ function client_registration_form() {
     $id = $_GET['clientID'];
     $post_id = get_the_ID();
     $cm = get_post_meta($post_id, "client_profile_" . $id);
-    $cm = unserialize($cm);
-    // $client_meta = get_post_meta($post_id, "client_profile_" . $id);
-    // foreach($client_meta as $cm) { 
-    //     $cm = unserialize($cm); 
-    // }
     
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if(isset($_POST)) {
             /** Add or update client profile in meta table */
-            if (! add_post_meta($post_id, "client_profile_" . $id, serialize($_POST), true)) {
-                update_post_meta($post_id, "client_profile_" . $id, serialize($_POST));
+            if (! add_post_meta($post_id, "client_profile_" . $id, $_POST, true)) {
+                update_post_meta($post_id, "client_profile_" . $id, $_POST);
             }
             /** get selected clients profile from meta table */
             $cm = get_post_meta($post_id, "client_profile_" . $id);
-            $cm = unserialize($cm);
         }
     }
     
@@ -763,11 +757,11 @@ function select_multiple($array, $name, $title, $col, $cm) {
 Standard text field
 ************************************************/
 function text_field($name, $title, $col, $cm) {
-    echo ("field name 0:: " .$cm[$name][0]);
-    echo ("field name:: " .$cm[$name]);
+    // echo ("field name 0:: " .$cm[$name][0]);
+    // echo ("field name:: " .$cm[$name]);
     echo '<div class="col-md-' . $col . '" >';
     echo '<label class="control-label">' . $title . '</label>';
-    echo '<input class="form-control" type="text" name="' . $name . '" pattern="[a-zA-Z0-9 ]+" value="' . $cm[$name][0]  . '" size="40" />';
+    echo '<input class="form-control" type="text" name="' . $name . '" pattern="[a-zA-Z0-9 ]+" value="' . $cm[$name]  . '" size="40" />';
     echo '</div>';
 }
 
