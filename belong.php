7246b75133d520487e34085209d02781edf6754f
Javid Yousaf <jav@Javids-MacBook-Pro.local>
2017-10-25 11:12:20 +0100

    d

--- belong.php
+++ belong.php
@@ -5,7 +5,7 @@
 * Plugin URI: http://belong-horizon.cloudapp.net
 * Bitbucket Plugin URI: https://javidyousaf@bitbucket.org/javidyousaf/belong.git
 * Description: Custom functionality for Belong Nottingham CRM
-* Version: 0.3.3.3
+* Version: 0.3.3.4
 * Author: Javid Yousaf
 * License: GPL3
 */
@@ -966,7 +966,7 @@ function children($cm) {
           var html = [];
 
 
-        if(typeof cm["pw-child-name"] != 'undefined') {
+        if(cm.hasOwnProperty('pw-child-name')) {
             console.log("CM variable: " + cm);
             var child_names = cm["pw-child-name"];
             var child_dobs = cm["pw-child-dob"];
