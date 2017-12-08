0d0bb46a1864bb32984abec94306ff574fde2679
Javid Yousaf <jav@Javids-MacBook-Pro.local>
2017-12-08 10:18:45 +0000

    s

--- scheduler.php
+++ scheduler.php
@@ -5,9 +5,9 @@
 * This will execute daily and send EMAIL/SMS notifications to clients
 **********************************************************/
 
-function belong_get_assignments() {
+function belong_send_notifications() {
     ob_start();
-    echo "Currently reminders triggered each time this page is refreshed<br />";
+    echo "REMINDERS<br /><br />";
     $assignment_args = array(
     'posts_per_page' => -1,
     'post_type' => 'assignments'
@@ -23,11 +23,12 @@ function belong_get_assignments() {
             $assignment_reminder_period = get_field('assignment_reminder_period', $post->ID);
             $assignment_reminder_type = get_field('assignment_reminder_type', $post->ID);
             $assignment_users = get_field('assignment_client', $post->ID); //multi select array of clients
-            echo $assignment_users[0]['user_email'];
+            // echo $assignment_users[0]['user_email'];
 
             if ($assignment_type == "Modules") {
                 $complete_by = get_field('assignment_complete_by', $post->ID);
                 $complete_date = new DateTime($complete_by);
+                echo "Calling sendReminder() for MODULES <br />";
                 sendReminders($complete_date, $assignment_reminder_period, $assignment_users, $assignment_reminder_type);
             }
 
@@ -35,28 +36,26 @@ function belong_get_assignments() {
                 $assignment_event = get_field('assignment_select_event', $post->ID);
                 $event_datetime   = get_field('event_date', $assignment_event->ID);
                 $event_date       = new DateTime($event_datetime);
+                echo "Calling sendReminder() for EVENTS <br />";
                 sendReminders($event_date, $assignment_reminder_period, $assignment_users, $assignment_reminder_type);
             }
-
         }
     }
     return ob_get_clean();
 }
-add_shortcode('belong_assignments', 'belong_get_assignments');
-
+add_shortcode('belong_notifications', 'belong_send_notifications');
 
 function sendReminders($date, $reminder_period, $users, $reminder_type) {
     if (isReminderTriggered($date, $reminder_period)) {
-        $email_addresses = $users[0]['user_email']; //***** */
+        $email_addresses = $users[0]['user_email'];
         if ($reminder_type == "email") {
-            echo "EMAIL <br />";
-            
+            echo "send reminders for EMAIL <br />";
             belong_send_emails("Pathways test message body.", "Reminder", $email_addresses);
         } else if ($reminder_type == "sms") {
             echo "SMS <br />";
             // belong_send_SMS($message, $numbers);
         } else if ($reminder_type == "both") {
-            echo "BOTH <br />";
+            echo "send reminders for BOTH <br />";
             belong_send_emails("Pathways test message body.", "Reminder", $email_addresses);
             // belong_send_SMS($message, $numbers);
         }                   
