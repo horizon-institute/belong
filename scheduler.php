7e684e13a1a1b0da648d1e98b34bc49fb37ffc5d
Javid Yousaf <jav@ppshorizon1203.nottingham.ac.uk>
2017-12-13 14:40:41 +0000

    correect relative path for scheduler

--- scheduler.php
+++ scheduler.php
@@ -1,6 +1,6 @@
 <?php
-
-require_once("../../../wp-load.php");
+$parse_uri = explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] );
+require_once( $parse_uri[0] . 'wp-load.php' );
 /*********************************************************
 * Scheduled job for Belong notifications
 * This will execute daily and send EMAIL/SMS notifications
