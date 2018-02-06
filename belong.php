c38da8b947c2661d121ba196105b69fca5415e9c
Kevin Glover <ktg@cs.nott.ac.uk>
2018-02-06 14:33:37 +0000

    jquery ui cdn fix part 2

--- belong.php
+++ belong.php
@@ -5,7 +5,7 @@
  * Plugin URI: http://belong-horizon.cloudapp.net
  * GitHub Plugin URI: https://github.com/horizon-institute/belong.git
  * Description: Custom functionality for Belong Nottingham CRM
- * Version: 0.4.9.5
+ * Version: 0.4.9.6
  * Author: Javid Yousaf
  * License: GPL3
  */
@@ -14,9 +14,9 @@
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 wp_enqueue_script( 'jquery' );
-wp_enqueue_script( 'jquery-ui-core' );
+wp_enqueue_script( 'jquery-ui-core' , 'https://code.jquery.com/ui/1.8.24/jquery-ui.min.js' );
 wp_enqueue_script( 'jquery-ui-datepicker' );
-wp_enqueue_style( 'jquery-ui-css', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css' );
+wp_enqueue_style( 'jquery-ui-css', 'https://code.jquery.com/ui/1.8.24/themes/smoothness/jquery-ui.css' );
 wp_register_script( 'prefix_bootstrap', '//netdna.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' );
 wp_enqueue_script( 'prefix_bootstrap' );
 wp_register_style( 'prefix_bootstrap', '//netdna.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' );
